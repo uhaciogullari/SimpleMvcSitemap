2c517482091a36bb2125c45dbb9c5a258f439220
Ziya SARIKAYA <sarikayaziya@gmail.com>
2013-12-29 09:35:59 +0200

    Added ImageDefinition for  supporting image sitemaps

---
+++ SimpleMvcSitemap/ImageDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace SimpleMvcSitemap
+{
+    public class ImageDefinition
+    {
+        [XmlElement("caption", Order = 1, Namespace = SitemapNamespaceConstants.IMAGE)]
+        public string Caption { get; set; }
+
+        [XmlElement("title", Order = 2, Namespace = SitemapNamespaceConstants.IMAGE)]
+        public string Title { get; set; }
+
+        [XmlElement("loc", Order = 3, Namespace = SitemapNamespaceConstants.IMAGE)]
+        public string Url { get; set; }
+    }
+}
