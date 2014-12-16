18b5dd31dc5af016d429341eee72daea43c3063d
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2014-12-16 14:30:22 +0200

    Added ReSharper annotation

--- SimpleMvcSitemap.Tests/XmlAssertionExtensions.cs
+++ SimpleMvcSitemap.Tests/XmlAssertionExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using System.Xml;
 using FluentAssertions;
 using FluentAssertions.Primitives;
+using JetBrains.Annotations;
 
 namespace SimpleMvcSitemap.Tests
 {
     public static class XmlAssertionExtensions
     {
-        public static void BeXmlEquivalent(this StringAssertions assertions, string filename)
+        public static void BeXmlEquivalent(this StringAssertions assertions, [PathReference]string filename)
         {
             XmlDocument doc = new XmlDocument { PreserveWhitespace = false };
             doc.Load(filename);
