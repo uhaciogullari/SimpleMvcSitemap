3483b3ad922bf1571b4057e732a366449aa7604e
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2014-12-25 15:45:45 +0200

    Implemented ReflectionHelper

---
+++ SimpleMvcSitemap.Tests/ReflectionHelperTests.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SimpleMvcSitemap.Tests
+{
+    public class ReflectionHelperTests : TestBase
+    {
+        private IReflectionHelper _reflectionHelper;
+
+        protected override void FinalizeSetUp()
+        {
+            _reflectionHelper = new ReflectionHelper();
+        }
+
+        private class SampleType1 { }
+
+        [Test]
+        public void GetUrlProperties_ClassHasNoProperties_DoesNotThrowException()
+        {
+            _reflectionHelper.GetPropertyModel(typeof(SampleType1)).Should().NotBeNull();
+        }
+
+
+        private class SampleType2
+        {
+            [Url]
+            public string Url { get; set; }
+
+            public string Title { get; set; }
+
+            [Url]
+            public string Url4 { get { return null; } }
+
+            [Url]
+            public string Url5 { set { } }
+
+            [Url]
+            public int Url2 { get; set; }
+
+            [Url]
+            public SampleType2 Url3 { get; set; }
+        }
+
+        [Test]
+        public void GetUrlProperties_ClassHasUrlProperties_ReturnUrlProperty()
+        {
+            UrlPropertyModel urlPropertyModel = _reflectionHelper.GetPropertyModel(typeof(SampleType2));
+
+            urlPropertyModel.UrlProperties.Should().HaveCount(1).And.ContainSingle(info => info.Name == "Url");
+        }
+
+        private class SampleType3
+        {
+            public List<SampleType1> List1 { get; set; }
+            [Url]
+            public SampleType1[] List2 { get; set; }
+            public SampleType1 Item { get; set; }
+            public IEnumerable List3 { get; set; }
+            public IEnumerable<SampleType2> List4 { set { } }
+        }
+
+        [Test]
+        public void GetUrlProperties_ClassHasEnumerableProperties_FindsEnumerableProperties()
+        {
+            UrlPropertyModel urlPropertyModel = _reflectionHelper.GetPropertyModel(typeof(SampleType3));
+
+            urlPropertyModel.UrlProperties.Should().BeEmpty();
+            urlPropertyModel.EnumerableProperties.Should().HaveCount(3);
+        }
+
+        private class SampleType4
+        {
+            public SampleType1 SampleType1 { get; set; }
+            [Url]
+            public SampleType2 SampleType2 { get; set; }
+
+            public string S { get; set; }
+
+            public SampleType3 SampleType3 { set { } }
+        }
+
+        [Test]
+        public void GetUrlProperties_ClassHasClassProperties_FindsClassProperties()
+        {
+            UrlPropertyModel urlPropertyModel = _reflectionHelper.GetPropertyModel(typeof(SampleType4));
+
+            urlPropertyModel.UrlProperties.Should().BeEmpty();
+            urlPropertyModel.ClassPropeties.Should().HaveCount(2);
+        }
+
+    }
+}
--- SimpleMvcSitemap/ReflectionHelper.cs
+++ SimpleMvcSitemap/ReflectionHelper.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
 
 namespace SimpleMvcSitemap
 {
@@ -6,7 +9,31 @@ namespace SimpleMvcSitemap
     {
         public UrlPropertyModel GetPropertyModel(Type type)
         {
-            throw new NotImplementedException();
+            UrlPropertyModel result = new UrlPropertyModel();
+            PropertyInfo[] propertyInfos = type.GetProperties();
+
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                if (propertyInfo.GetCustomAttributes(typeof (UrlAttribute), true).Any() &&
+                    propertyInfo.PropertyType == typeof(string) &&
+                    propertyInfo.CanRead &&
+                    propertyInfo.CanWrite)
+                {
+                    result.UrlProperties.Add(propertyInfo);
+                }
+                else if (typeof(IEnumerable).IsAssignableFrom(propertyInfo.PropertyType) && propertyInfo.CanRead)
+                {
+                    result.EnumerableProperties.Add(propertyInfo);
+
+                }
+                else if (propertyInfo.PropertyType.IsClass && propertyInfo.PropertyType != typeof(string) && propertyInfo.CanRead)
+                {
+                    result.ClassPropeties.Add(propertyInfo);
+                }
+            }
+
+
+            return result;
         }
     }
 }
