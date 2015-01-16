6b7362b0997053a81b5b9eeb05a0d8d2efe4fb43
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2015-01-16 11:25:17 +0200

    Incremented version to 2.3.1

--- SimpleMvcSitemap/Properties/AssemblyInfo.cs
+++ SimpleMvcSitemap/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default tgithe Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.3.1")]
+[assembly: AssemblyFileVersion("2.3.1")]
