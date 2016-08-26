ce552e78441a131e249ca176f6000e8f42a0bdb4
Furkan Cüneyt BEKAR <f.cuneytbekar@hotmail.com>
2016-08-26 15:33:04 +0300

    PageIndex Generation Reverted

--- src/SimpleMvcSitemap.Sample/SampleBusiness/SitemapConfiguration.cs
+++ src/SimpleMvcSitemap.Sample/SampleBusiness/SitemapConfiguration.cs
@@ -6,18 +6,20 @@ namespace SimpleMvcSitemap.Sample.SampleBusiness
     {
         private readonly UrlHelper _urlHelper;
 
-        public SitemapConfiguration(int? currentPage, UrlHelper urlHelper)
+        public SitemapConfiguration(int? currentPage, UrlHelper urlHelper, bool? revertIndex)
         {
             _urlHelper = urlHelper;
             CurrentPage = currentPage;
             Size = 1;
+	        RevertIndex = revertIndex ?? false;
         }
 
         public int? CurrentPage { get; private set; }
 
         public int Size { get; private set; }
+	    public bool RevertIndex { get; private set; }
 
-        public string CreateSitemapUrl(int currentPage)
+	    public string CreateSitemapUrl(int currentPage)
         {
             return _urlHelper.Action("StaticPages", "Home", new { id = currentPage });
         }
