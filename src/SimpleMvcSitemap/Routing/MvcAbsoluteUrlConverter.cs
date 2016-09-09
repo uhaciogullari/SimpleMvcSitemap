976ef4d658c4975083a2e93b08e882f9c78b053d
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2016-09-10 01:32:11 +0200

    Implemented MvcAbsoluteUrlConverter

---
+++ src/SimpleMvcSitemap/Routing/MvcAbsoluteUrlConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleMvcSitemap.Routing
+{
+    public class MvcAbsoluteUrlConverter : AbsoluteUrlConverterBase, IAbsoluteUrlConverter
+    {
+        private readonly HttpContextBase _httpContext;
+
+        public MvcAbsoluteUrlConverter(HttpContextBase httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+
+        public string ConvertToAbsoluteUrl(string relativeUrl)
+        {
+            HttpRequestBase request = _httpContext.Request;
+            string baseUrl = $"{request.Url.Scheme}://{request.Url.Authority}{UrlHelper.GenerateContentUrl("~", _httpContext)}".TrimEnd('/');
+
+            return CreateAbsoluteUrl(baseUrl, relativeUrl);
+        }
+    }
+}
