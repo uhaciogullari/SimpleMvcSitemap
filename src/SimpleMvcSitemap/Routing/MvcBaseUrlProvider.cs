644cc3aa32b5b2b1dda655918740d7de8d8a420b
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2016-09-25 19:24:20 +0200

    Implemented MvcBaseUrlProvider

---
+++ src/SimpleMvcSitemap/Routing/MvcBaseUrlProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleMvcSitemap.Routing
+{
+    class MvcBaseUrlProvider : IBaseUrlProvider
+    {
+        private HttpContextBase httpContext;
+
+        public MvcBaseUrlProvider(HttpContextBase httpContext)
+        {
+            this.httpContext = httpContext;
+
+        }
+
+        public string BaseUrl => $"{httpContext.Request.Url.Scheme}://{httpContext.Request.Url.Authority}{httpContext.Request.ApplicationPath}";
+    }
+}
