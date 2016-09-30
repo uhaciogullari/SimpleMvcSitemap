fbf4b2ec84e3bf81daf6779a0e0ed332052c7703
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2016-09-30 23:22:30 +0200

    Added comments for IBaseUrlProvider

--- src/SimpleMvcSitemap/Routing/IBaseUrlProvider.cs
+++ src/SimpleMvcSitemap/Routing/IBaseUrlProvider.cs
@@ -2,8 +2,15 @@
 
 namespace SimpleMvcSitemap.Routing
 {
+    /// <summary>
+    /// An abstraction for retrieving the Base URL that will be
+    /// used for creating absolute URLs
+    /// </summary>
     public interface IBaseUrlProvider
     {
+        /// <summary>
+        /// Gets the base URL.
+        /// </summary>
         Uri BaseUrl { get; }
     }
 }
